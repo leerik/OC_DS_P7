1cb64d328b4213aa38e4626851190bbfc5ff57d7
leerik <wendlingeric@yahoo.ca>
2020-08-17 00:04:29 +0200

    Modif chemin d'accès

--- P7_03_dashboard.py
+++ P7_03_dashboard.py
@@ -55,8 +55,8 @@ colors = {'background_1': '#FFFFFF', # blanc
           'background_2': '#C0C0C0', # gris clair
           'text_2': '#FF0000'} # rouge
 
-path_features = '../features_app/'
-path_results = '../results_app/'
+path_features = './features_app/'
+path_results = './results_app/'
 
 ##########################
 ### Import des données ###
